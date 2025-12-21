a40d8a4f4f510cd165a1870c584006d4b406e63c
AMEEN <ameenoday900@gmail.com>
2025-12-21 21:32:04 +0800

    oop

--- app/components/navbarsecond.tsx
+++ app/components/navbarsecond.tsx
@@ -15,7 +15,7 @@ const navitemsright = [
 
     { lable: "get notion calendar for free", href: "#" },
     { lable: "tester one", href: "/" }
-]
+];
 const navitemsleft = {
     image: { src: "/nlogo.png", alt: "logo" }
 }
@@ -25,22 +25,25 @@ const navitemsleft = {
 
 const Navbarsecond = () => {
     return (
-        <nav className="px-4 py-2 sticky top-0 ">
+        <nav className="px-4 py-2 sticky top-0 justify-between ">
             <div className="grid grid-cols-3">
-                <div className="items">
-                    <Image className=""
-                        src={navitemsleft.image}
-                        width={ }
-                        height={ }
-                        alt=""
-                    >
+                <div className="flex justify-self-start px-4 py-2">
+                    <Link href="/">
+                        <Image className=""
+                            src={navitemsleft.image.src}
+                            width={50}
+                            height={50}
+                            alt={navitemsleft.image.alt}
+                        >
 
-                    </Image>
+                        </Image>
+                    </Link>
 
                 </div>
-                {navitems.map((items, index) => (
 
-                    <div>
+
+                <div className="flex gap-5 overflow-hidden w-[1000px]">
+                    {navitems.map((items, index) => (
                         <Link
                             className="hover:bg-gray-500 rounded-md px-1 py-1"
                             key={index}
@@ -49,8 +52,21 @@ const Navbarsecond = () => {
                         >
                             {items.lable}
                         </Link>
-                    </div>
-                ))}
+                    ))}
+                </div>
+                <div className="justify-self-end py-2">
+                    {navitemsright.map((items, index) => (
+
+                        <Link
+                            className=""
+                            key={index}
+                            href={items.href}>
+                            {items.lable}
+                        </Link>
+
+                    ))}
+                </div>
+
             </div>
         </nav >
 
